04f2d933e523f3e16065f9be7a13bc5ec5c8f9e9
dplucky <53617038+dplucky@users.noreply.github.com>
2019-10-12 16:38:23 +0800

    tab栏切换

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
     <!-- 幻灯片 -->
+    <!-- interval:相隔时间 -->
+    <!-- arrow:是否显示左右的箭头 -->
     <el-carousel
       :interval="5000"
       arrow="always"
@@ -19,6 +21,43 @@
         </div>
       </el-carousel-item>
     </el-carousel>
+    <!-- 搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar">
+
+        <!-- tab栏 -->
+        <el-row
+          type="flex"
+          class="search-tab"
+        >
+          <span
+            v-for="(item, index) in options"
+            :key="index"
+            :class="{active: current === index}"
+            @click="handleTabChange(index)"
+          >
+            <i>{{item.title}}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row
+          type="flex"
+          align="middle"
+          class="search-input"
+        >
+          <input
+            :placeholder="options[current].placeholder"
+            v-model="searchValue"
+            @keyup.enter="handleSearch"
+          />
+          <i
+            class="el-icon-search"
+            @click="handleSearch"
+          ></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,17 +66,43 @@ export default {
   data() {
     return {
       // 轮播图数据
-      banners: []
+      banners: [],
+      // tab的数据结构
+      options: [      // 搜索框tab选项
+        {
+          title: "攻略",
+          placeholder: "搜索城市",
+        },
+        {
+          title: "酒店",
+          placeholder: "请输入城市搜索酒店",
+        },
+        {
+          title: "机票",
+          placeholder: "",
+        }
+      ],
+      // tab栏的索引
+      current: 0
     }
   },
   mounted() {
     this.$axios({
       url: "/scenics/banners"
     }).then(res => {
-      const { data } = res.data;
-      this.banners = data;
+      const { data } = res.data
+      this.banners = data
     })
-  }
+  },
+  methods: {
+    handleTabChange(index) {
+      this.current = index;
+      if (index === 2) {
+        this.$router.push("/air")
+      }
+    },
+    handleSearch() { }
+  },
 }
 </script>
 
@@ -54,6 +119,95 @@ export default {
   .banner-image {
     width: 100%;
     height: 100%;
+  }
+
+  .banner-content {
+    z-index: 9;
+    width: 1000px;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    margin-left: -500px;
+    border-top: 1px transparent solid;
+
+    .search-bar {
+      width: 552px;
+      margin: 0 auto;
+    }
+
+    .search-tab {
+      .active {
+        i {
+          color: #333;
+        }
+        &::after {
+          background: #eee;
+        }
+      }
+
+      span {
+        width: 82px;
+        height: 36px;
+        display: block;
+        position: relative;
+        margin-right: 8px;
+        cursor: pointer;
+
+        i {
+          position: absolute;
+          z-index: 2;
+          display: block;
+          width: 100%;
+          height: 100%;
+          line-height: 30px;
+          text-align: center;
+          color: #fff;
+        }
+
+        &:after {
+          position: absolute;
+          left: 0;
+          top: 0;
+          display: block;
+          content: "";
+          width: 100%;
+          height: 100%;
+          border: 1px rgba(255, 255, 255, 0.2) solid;
+          border-bottom: none;
+          transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+          transform-origin: bottom left;
+          background: rgba(0, 0, 0, 0.5);
+          border-radius: 1px 2px 0 0;
+          box-sizing: border-box;
+        }
+      }
+    }
+
+    .search-input {
+      width: 550px;
+      height: 46px;
+      background: #fff;
+      border-radius: 0 4px 4px 4px;
+      border: 1px rgba(255, 255, 255, 0.2) solid;
+      border-top: none;
+      box-sizing: unset;
+
+      input {
+        flex: 1;
+        height: 20px;
+        padding: 13px 15px;
+        outline: none;
+        border: 0;
+        font-size: 16px;
+      }
+
+      .el-icon-search {
+        cursor: pointer;
+        font-size: 22px;
+        padding: 0 10px;
+        font-weight: bold;
+      }
+    }
   }
 }
 </style>
